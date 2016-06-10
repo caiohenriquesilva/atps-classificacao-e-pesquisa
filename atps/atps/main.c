a04a42ad61af2c26046a0b529ab1f9d6e2016994
Caio Henrique <Caio Henrique>
2016-06-10 00:45:57 -0300

    inclusao dos novos metodos de ordenacao

--- atps/atps/main.c
+++ atps/atps/main.c
@@ -40,7 +40,9 @@ long current_timestamp() {
 int buscaMedidaLinear(int medida)
 {
     long start = current_timestamp();
-    for (int i = 0; i < 1000; i++) {
+
+    int i;
+    for (i = 0; i < 1000; i++) {
         if (vetResidencias[i].medidaConsumo == medida) {
             long end = current_timestamp();
             long time = end - start;
@@ -48,81 +50,220 @@ int buscaMedidaLinear(int medida)
             return i;
         }
     }
-    
+
     long end = current_timestamp();
     long time = end - start;
     printf("\n\nEsta consulta levou %ldms", time);
-    
+
     return -1;
 }
 
 void bubbleSort() {
-    int aux;
-
-    for (int i = 0; i < 1000; i++) {
-        if (vetResidencias[i].medidaConsumo > vetResidencias[i+1].medidaConsumo) {
-            aux = vetResidencias[i+1].medidaConsumo;
-            vetResidencias[i+1].medidaConsumo = vetResidencias[i].medidaConsumo;
-            vetResidencias[i].medidaConsumo = aux
+    int aux, j, i;
+    for (j = 999; j >= 1; j--) {
+        for (i = 0; i < 1000; i++) {
+            if (vetResidencias[i].medidaConsumo > vetResidencias[i+1].medidaConsumo) {
+                aux = vetResidencias[i].medidaConsumo;
+                vetResidencias[i].medidaConsumo = vetResidencias[i+1].medidaConsumo;
+                vetResidencias[i+1].medidaConsumo = aux;
+            }
         }
+    }
+
+    int m;
+    for (m = 0; m < 1000; m++) {
+        printf("%.0f, ", vetResidencias[m].medidaConsumo);
     }
 }
 
 void insertionSort() {
-    
+    int i, j, atual;
+
+    for (i = 1; i < 1000; i++)
+    {
+        atual = vetResidencias[i].medidaConsumo;
+        j = i - 1;
+
+        while ((j >= 0) && (atual < vetResidencias[j].medidaConsumo))
+        {
+            vetResidencias[j + 1].medidaConsumo = vetResidencias[j].medidaConsumo;
+            j = j - 1;
+        }
+
+        vetResidencias[j + 1].medidaConsumo = atual;
+    }
 }
 
 void selectionSort() {
-
+    int i, j, min, aux;
+    for (i = 0; i < 999; i++)
+    {
+        min = i;
+        for (j = (i+1); j < 1000; j++) {
+            if(vetResidencias[j].medidaConsumo < vetResidencias[min].medidaConsumo)
+                min = j;
+        }
+        if (i != min) {
+            aux = vetResidencias[i].medidaConsumo;
+            vetResidencias[i].medidaConsumo = vetResidencias[min].medidaConsumo;
+            vetResidencias[min].medidaConsumo = aux;
+        }
+    }
 }
 
-void mergeSort() {
-    
+void mergeSort(int * V, int inicio, int fim) {
+    int i, j, k, metadeTamanho, *V_temp;
+
+    if(inicio == fim) {
+        return;
+    }
+
+    metadeTamanho = (inicio + fim ) / 2;
+    mergeSort(V, inicio, metadeTamanho);
+    mergeSort(V, metadeTamanho + 1, fim);
+
+    i = inicio;
+    j = metadeTamanho + 1;
+    k = 0;
+    V_temp = (int *) malloc(sizeof(int) * (fim - inicio + 1));
+
+    while(i < metadeTamanho + 1 || j  < fim + 1) {
+        if (i == metadeTamanho + 1 ) {
+            V_temp[k] = V[j];
+            j++;
+            k++;
+        }
+        else {
+            if (j == fim + 1) {
+                V_temp[k] = V[i];
+                i++;
+                k++;
+            }
+            else {
+                if (V[i] < V[j]) {
+                    V_temp[k] = V[i];
+                    i++;
+                    k++;
+                }
+                else {
+                    V_temp[k] = V[j];
+                    j++;
+                    k++;
+                }
+            }
+        }
+
+    }
+
+    for(i = inicio; i <= fim; i++) {
+        V[i] = V_temp[i - inicio];
+    }
+
+    free(V_temp);
 }
 
+void quickSort(int vet[], int esq, int dir) {
+    int pivo = esq,i,ch,j;
+    for(i=esq+1;i<=dir;i++){
+        j = i;
+        if(vet[j] < vet[pivo]){
+         ch = vet[j];
+         while(j > pivo){
+            vet[j] = vet[j-1];
+            j--;
+         }
+         vet[j] = ch;
+         pivo++;
+        }
+    }
+    if(pivo-1 >= esq){
+        quick(vet,esq,pivo-1);
+    }
+    if(pivo+1 <= dir){
+        quick(vet,pivo+1,dir);
+    }
+}
 
+void heapSort(int a[], int n) {
+    void heapsort(int a[], int n) {
+   int i = n / 2, pai, filho, t;
+   for (;;) {
+      if (i > 0) {
+          i--;
+          t = a[i];
+      } else {
+          n--;
+          if (n == 0) return;
+          t = a[n];
+          a[n] = a[0];
+      }
+      pai = i;
+      filho = i * 2 + 1;
+      while (filho < n) {
+          if ((filho + 1 < n)  &&  (a[filho + 1] > a[filho]))
+              filho++;
+          if (a[filho] > t) {
+             a[pai] = a[filho];
+             pai = filho;
+             filho = pai * 2 + 1;
+          } else {
+             break;
+          }
+      }
+      a[pai] = t;
+   }
+}
+}
 
 
 
 int main(int argc, const char * argv[])
 {
-    int vetorMedidas[1000];
-    for (int i = 0; i < 1000; i++) {
+    int vetorMedidas[1000], i;
+    for (i = 0; i < 1000; i++) {
         vetorMedidas[i] = randomInteger(1, 99999);
         vetResidencias[i].medidaConsumo = vetorMedidas[i];
         vetResidencias[i].numCasa = i;
         printf("%d, ", vetorMedidas[i]);
     }
-    
+
     int sair, valor;
     do {
-        
-        
+
+
         int escolha, escolhaOrd, sairOrd;
         printf("\n\n\nDigite a opcao desejada: \n1. Pesquisa\n2. Ordenacao\n3. Sair\n>> ");
         scanf("%d", &escolha);
-        
+
         switch (escolha) {
             case 1:
-                
+
                 printf("Digite o valor de medida a ser pesquisado: ");
                 scanf("%ud", &valor);
-                
+
                 int resultBuscaLinear = buscaMedidaLinear(valor);
-                
+
                 if (resultBuscaLinear > 0) {
                     printf("\n\nFoi encontrada a casa de numero %d com o valor %d de medida", resultBuscaLinear, valor);
                 } else {
                     printf("\n\nNenhuma casa foi encontrada");
                 }
-                
+
                 break;
             case 2:
-                
+
                 do {
-                    printf("Digite o algoritmo de ordenação desejado: \n1. Bubble Sorte \n2. Insertion Sort \n3. Selection Sorte \n4. Merge Sort \n5. Sair\n>> ");
+                    printf("Digite o algoritmo de ordenação desejado:");
+                    printf("\n1. Bubble Sort");
+                    printf("\n2. Insertion Sort");
+                    printf("\n3. Selection Sort");
+                    printf("\n4. Merge Sort");
+                    printf("\n5. Quick Sort");
+                    printf("\n6. Heap Sort");
+                    printf("\n7. Sair\n");
+                    printf("");
                     scanf("%d", &escolhaOrd);
-                    
+
                     switch (escolhaOrd) {
                         case 1:
                             bubbleSort();
@@ -137,6 +278,12 @@ int main(int argc, const char * argv[])
                             mergeSort();
                             break;
                         case 5:
+                            quickSort();
+                            break;
+                        case 6:
+                            heapSort();
+                            break;
+                        case 7:
                             return 0;
                             break;
                         default:
@@ -144,7 +291,7 @@ int main(int argc, const char * argv[])
                             break;
                     }
                 } while (sairOrd == 0);
-                
+
                 break;
             case 3:
                 return 0;
@@ -153,10 +300,7 @@ int main(int argc, const char * argv[])
                 printf("Opcao incorreta, tente novamente! \n\n\n");
                 break;
         }
-        
-        
-        
     } while(sair == 0);
-    
+
     return 0;
 }
